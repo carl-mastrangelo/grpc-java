786a96f6bf14035ca81849285ad5a65a4fe2dd91
Carl Mastrangelo <notcarl@google.com>
2016-04-28 12:42:16 -0700

    Force hang

--- examples/src/main/java/io/grpc/examples/helloworld/NoiseClient.java
+++ examples/src/main/java/io/grpc/examples/helloworld/NoiseClient.java
@@ -1,3 +1,34 @@
+/*
+ * Copyright 2015, Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ *    * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package io.grpc.examples.helloworld;
 
 import io.grpc.CallOptions;
@@ -24,8 +55,9 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
 public class NoiseClient {
-  private final static int COUNT = 1000000000;
+  private  static final int COUNT = 1000000000;
 
+  /** Checkstyle sucks. */
   public static void main(String []args) throws Exception {
     ExecutorService es = Executors.newFixedThreadPool(2);
 
@@ -41,32 +73,40 @@ public class NoiseClient {
             .executor(es)
             .usePlaintext(true)
             .build();
-    final Semaphore sem = new Semaphore(10);
+    final Semaphore sem = new Semaphore(1);
     final CountDownLatch latch = new CountDownLatch(COUNT);
 
 
     for (int i = 0; i < COUNT; i++) {
-      sem.acquire();
-
       final ClientCall<Void, Void> call = channel.newCall(METHOD, CallOptions.DEFAULT);
       ClientCall.Listener<Void> listen =
           new ClientCall.Listener<Void>() {
+          @Override
+          public void onHeaders(Metadata headers) {
+            System.err.println("headers " + call);
+
+          }
+
             @Override
             public void onMessage(Void message) {
+              System.err.println("cancelled " + call);
               call.cancel();
-
             }
+
             @Override
             public void onClose(Status status, Metadata trailers) {
-              System.out.println(trailers);
+              System.err.println("closed " + call);
               latch.countDown();
               sem.release();
             }
           };
+      sem.acquire();
       call.start(listen, new Metadata());
+      System.err.println("started " + call);
       call.sendMessage(null);
       call.halfClose();
       call.request(1);
+      System.err.println("sent " + call);
     }
 
     latch.await();
@@ -115,7 +155,9 @@ public class NoiseClient {
                   call.sendHeaders(new Metadata());
                   call.request(1);
                   call.sendMessage(null);
+
                   return new ServerCall.Listener<Void>() {
+
 
                     @Override
                     public void onCancel() {
